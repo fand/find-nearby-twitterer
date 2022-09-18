98c05d2ab2d3061529d089c03708c159d6aab3ea
fand <mail@amagi.dev>
2022-09-18 16:13:50 +0900

    feat: oauth

---
+++ src/auth.rs
@@ -0,0 +1,98 @@
+use chrono::Utc;
+use std::collections::HashMap;
+
+extern crate base64;
+use hmacsha1::hmac_sha1;
+use percent_encoding::{utf8_percent_encode, AsciiSet, PercentEncode, NON_ALPHANUMERIC};
+
+const FRAGMENT: &AsciiSet = &NON_ALPHANUMERIC
+    .remove(b'~')
+    .remove(b'-')
+    .remove(b'.')
+    .remove(b'_');
+
+fn encode(input: &str) -> PercentEncode {
+    utf8_percent_encode(input, FRAGMENT)
+}
+
+fn make_signature_base_string(method: &str, url: &str, parameter_string: &str) -> String {
+    format!(
+        "{}&{}&{}",
+        encode(method),
+        encode(url),
+        encode(parameter_string),
+    )
+}
+
+fn make_signature_key(oauth_consumer_secret: &str, oauth_token_secret: &str) -> String {
+    let oauth_consumer_secret = encode(oauth_consumer_secret);
+    let oauth_token_secret = encode(oauth_token_secret);
+    format!("{}&{}", oauth_consumer_secret, oauth_token_secret)
+}
+
+fn encode_signature(key: &str, data: &str) -> String {
+    let hash = hmac_sha1(key.as_bytes(), data.as_bytes());
+    base64::encode(&hash)
+}
+
+fn make_parameter_string(params: &HashMap<&str, &str>) -> String {
+    let mut params = params
+        .into_iter()
+        .map(|(k, v)| format!("{}={}", encode(k), encode(v)))
+        .collect::<Vec<String>>();
+    params.sort();
+    params.join("&")
+}
+
+pub struct Signature {
+    pub signature: String,
+    pub auth_header: String,
+}
+
+pub fn make_signature<'a>(
+    url: &str,
+    method: &str,
+    api_key: String,
+    api_key_secret: String,
+    access_token: String,
+    access_token_secret: String,
+    body: &Vec<(&'static str, &'static str)>,
+) -> Signature {
+    let timestamp = Utc::now().timestamp().to_string();
+
+    let mut params: HashMap<&str, &str> = HashMap::new();
+    params.insert("oauth_consumer_key", &api_key);
+    params.insert("oauth_nonce", &timestamp);
+    params.insert("oauth_timestamp", &timestamp);
+    params.insert("oauth_signature_method", "HMAC-SHA1");
+    params.insert("oauth_version", "1.0");
+    params.insert("oauth_token", &access_token);
+
+    for (k, v) in body {
+        params.insert(k, v);
+    }
+
+    let parameter_string = make_parameter_string(&params);
+
+    let signature_base_string = make_signature_base_string(method, url, &parameter_string);
+
+    let signature_key = make_signature_key(&api_key_secret, &access_token_secret);
+
+    let signature = encode_signature(&signature_key, &signature_base_string);
+
+    let auth_header = format!(
+        r#"OAuth oauth_nonce="{}", oauth_signature_method="{}", oauth_timestamp="{}", oauth_consumer_key="{}", oauth_signature="{}", oauth_version="{}", oauth_token="{}""#,
+        encode(params.get("oauth_nonce").unwrap()),
+        encode(params.get("oauth_signature_method").unwrap()),
+        encode(params.get("oauth_timestamp").unwrap()),
+        encode(params.get("oauth_consumer_key").unwrap()),
+        encode(&signature),
+        encode(params.get("oauth_version").unwrap()),
+        encode(&access_token),
+    );
+
+    Signature {
+        signature,
+        auth_header,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,141 @@
 use anyhow::Result;
-use kuon;
-use std::env;
 
 extern crate reqwest;
 use reqwest::Client;
+use reqwest::{header::AUTHORIZATION, Url};
+
+use serde::{Deserialize, Serialize};
+
+mod auth;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct UserJSON {
+    id: String,
+    name: String,
+    username: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DataJSON<T> {
+    data: T,
+}
+
+struct Twit {
+    client: Client,
+    bearer_token: String,
+}
+
+impl Twit {
+    pub fn new(bearer_token: String) -> Twit {
+        let client = Client::new();
+
+        Twit {
+            bearer_token: format!("Bearer {}", bearer_token),
+            client,
+        }
+    }
+
+    pub async fn get_user(&self, screen_name: &str) -> UserJSON {
+        let url_string = format!(
+            "https://api.twitter.com/2/users/by/username/{}",
+            screen_name
+        );
+
+        let body = vec![];
+
+        let api_key = std::env::var("TWITTER_API_KEY").unwrap();
+        let access_token = std::env::var("TWITTER_ACCESS_TOKEN").unwrap();
+        let api_key_secret = std::env::var("TWITTER_API_KEY_SECRET").unwrap();
+        let access_token_secret = std::env::var("TWITTER_ACCESS_TOKEN_SECRET").unwrap();
+
+        let signature = auth::make_signature(
+            url_string.as_str(),
+            "GET",
+            api_key,
+            api_key_secret,
+            access_token,
+            access_token_secret,
+            &body,
+        );
+
+        let url = Url::parse(&url_string).unwrap();
+        let client = Client::new()
+            .get(url)
+            .header(AUTHORIZATION, &signature.auth_header)
+            .form(&body);
+
+        let res = client
+            .send()
+            .await
+            .unwrap()
+            .json::<DataJSON<UserJSON>>()
+            .await
+            .unwrap();
+
+        res.data
+    }
+
+    pub async fn get_timeline(&self, user_id: String) -> String {
+        // let url = format!("https://api.twitter.com/2/user_timeline",);
+        let url_string = format!(
+            "https://api.twitter.com/2/users/{}/timelines/reverse_chronological",
+            user_id
+        );
+
+        let body = vec![];
+
+        let api_key = std::env::var("TWITTER_API_KEY").unwrap();
+        let access_token = std::env::var("TWITTER_ACCESS_TOKEN").unwrap();
+        let api_key_secret = std::env::var("TWITTER_API_KEY_SECRET").unwrap();
+        let access_token_secret = std::env::var("TWITTER_ACCESS_TOKEN_SECRET").unwrap();
+
+        let signature = auth::make_signature(
+            url_string.as_str(),
+            "GET",
+            api_key,
+            api_key_secret,
+            access_token,
+            access_token_secret,
+            &body,
+        );
+
+        let url = Url::parse(&url_string).unwrap();
+        let client = Client::new()
+            .get(url)
+            .header(AUTHORIZATION, &signature.auth_header)
+            .form(&body);
+
+        client.send().await.unwrap().text().await.unwrap()
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let url = format!(
-        "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=amagitakayosi",
-    );
+    let twit = Twit::new(std::env::var("TWITTER_BEARER_TOKEN").unwrap());
+    let user = twit.get_user("amagitakayosi").await;
+    let timeline = twit.get_timeline(user.id).await;
+    println!("{}", timeline);
 
-    let token = std::env::var("TWITTER_BEARER_TOKEN").unwrap();
-    let token = format!("Bearer {}", token);
-    let client = Client::new().get(&url).header("authorization", token);
+    // println!("{}", user.username);
 
-    let res = client.send().await.unwrap().text().await.unwrap();
-    println!("{}", res);
+    // let screen_name = "amagitakayosi";
+
+    // let url = format!(
+    //     "https://api.twitter.com/2/users/by?usernames={}",
+    //     screen_name
+    // );
+
+    // let url = format!(
+    //     "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=amagitakayosi",
+    // );
+
+    // let token = std::env::var("TWITTER_BEARER_TOKEN").unwrap();
+    // let token = format!("Bearer {}", token);
+    // let client = Client::new().get(&url).header("authorization", token);
+
+    // let res = client.send().await.unwrap().text().await.unwrap();
+    // println!("{}", res);
+
     Ok(())
 
     // println("{}", client);
